2e5c0f1b6501233b86046da158399e7c306e787f
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2026-06-11 09:22:21 +0200

    some functionality half way done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,11 @@ function HomePage() {
     return (
       <div>
         <h1>Home Page</h1>
+
+          <div>
+            <h3>Some functionality half way done...</h3>
+          </div>
+
       </div>
     );
   }
